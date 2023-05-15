7171d644e5b5f554d765deb27c4fa44cb6a4135c
cuong nguyen <chiendau1998@gmail.com>
2023-05-16 03:24:28 +0700

    ss

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,17 +30,37 @@ export class HomeComponent implements OnInit {
 				link: "https://google.com"
 			},
 			{
-				name: "Built Business Intelligence Dashboards for various Accounting Firms",
+				name: "Implemented Manage Engine for a 65-location Retailer",
 				img: "ảnh",
 				link: "https://google.com"
 			},
 			{
-				name: "Automated Data Sync between Certify and MIP for a Non-Profit Organization",
+				name: "Automated Invoice Review Process for a Mid-size CPA firm​",
 				img: "ảnh",
 				link: "https://google.com"
 			},
 			{
-				name: "Synchronized ERP system with Project Management System for a Mid-size CPA firm​",
+				name: "Performed Fraud Data Analytics for Government Agency",
+				img: "ảnh",
+				link: "https://google.com"
+			},
+			{
+				name: "Implemented CRM, PM, Data Warehouse and BI System for a Technology Company",
+				img: "ảnh",
+				link: "https://google.com"
+			},
+			{
+				name: "Performed Data Analysis for A Real Estate Management Holdings",
+				img: "ảnh",
+				link: "https://google.com"
+			},
+			{
+				name: "Implemented Salesforce for A Business Incubator",
+				img: "ảnh",
+				link: "https://google.com"
+			},
+			{
+				name: "Automated Reporting System for A Non Profit Organization",
 				img: "ảnh",
 				link: "https://google.com"
 			},
