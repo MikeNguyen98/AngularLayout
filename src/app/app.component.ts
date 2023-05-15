f95a9500010eb6451d1ec8eb988614f87e61a930
cuong nguyen <chiendau1998@gmail.com>
2023-05-16 02:34:20 +0700

    sss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ajax } from 'rxjs/ajax';
 import { map } from 'rxjs/operators'
 import { Constant } from './const/constant';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '';
   data:any;
+  url: string = "";
 	readonly CONSTANT = Constant
-  constructor() {
-     this.data = "";
-     this.title = "Using RxJs with Angular";
-     let a = this.getData();
+  constructor(private router : Router) {
   }
-
-  getData() {
-     const response =
-     ajax('https://jsonplaceholder.typicode.com/users')
-        .pipe(map(e => e.response));
-     response.subscribe(res => {
-        console.log(res);
-        this.data = res;
-     });
+  public href: string = "";
+  ngOnInit(): void {
+     console.log(this.router.url)
   }
-  
 }
